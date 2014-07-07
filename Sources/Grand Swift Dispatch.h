22a388cdb35c22016e41b2e1dc02cc2da5e733ef
Martin Kiss <martin.kiss@me.com>
2014-07-07 08:35:58 +0200

    Setup framework project
    Tests in thi Xcode are broken, so were removed

---
+++ Sources/Grand Swift Dispatch.h
@@ -0,0 +1,19 @@
+//
+//  Grand Swift Dispatch.h
+//  Grand Swift Dispatch
+//
+//  Created by Martin Kiss on 7.7.14.
+//  Copyright (c) 2014 Triceratops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Grand Swift Dispatch.
+FOUNDATION_EXPORT double Grand_Swift_DispatchVersionNumber;
+
+//! Project version string for Grand Swift Dispatch.
+FOUNDATION_EXPORT const unsigned char Grand_Swift_DispatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Grand_Swift_Dispatch/PublicHeader.h>
+
+
