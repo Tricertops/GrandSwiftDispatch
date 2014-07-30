8a6434522a6e32d97acdbf4a125cbe6a9c053120
Martin Kiss <martin.kiss@me.com>
2014-07-30 10:48:48 +0200

    Optional fallback operator

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -6,3 +6,8 @@ import Foundation
 let Yes = true
 let No = false
 
+//MARK: - Optionals
+func | <T>(optional: T?, fallback: @auto_closure () -> T) -> T {
+    return optional ? optional! : fallback()
+}
+
